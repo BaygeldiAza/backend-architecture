02bb4e2054dce7c52552e4194c3857a5b2e476ba
Baygeldi <baygeldi0718@gmail.com>
2025-11-09 01:11:56 +0800

    Commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
+import logo from './assets/logo.svg';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div style={{textAlign: "center", margin: "100px"}}>
-      <h1>You clicked {count} times </h1>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <img
+        src={logo}
+        alt="React logo"
+        style={{ width: "100px", height: "100px", animation: "spin 5s linear infinite" }}
+      />
+      <h1>You clicked {count} times</h1>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
     </div>
-  )
+  );
 }
