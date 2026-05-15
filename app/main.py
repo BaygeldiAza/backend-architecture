2603c432b0aa425dac0d63ea612017930d476e87
Baygeldi <baygeldi0718@gmail.com>
2026-05-15 14:13:05 +0800

    Router added

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
-    return {"message": "Fast API is running"}
+async def root():
+    return {"Welcome": "TO My API"}
+
+@app.get("/items/{item_id}")
+async def read_items(item_id: int, q: str | None=None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/createposts")
+def create_post(posts: dict = Body(...)):
+    return{"new_post": f"title {posts.title} content: {posts.content}"}
