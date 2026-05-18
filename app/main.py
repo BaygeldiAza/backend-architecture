1e4e90005f6fffc3773c48a391d582319676663c
Baygeldi <baygeldi0718@gmail.com>
2026-05-18 21:01:35 +0800

    Added schema

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    rating: Optional[int]
 
 @app.get("/")
 async def root():
@@ -13,5 +19,8 @@ async def read_items(item_id: int, q: str | None=None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/createposts")
-def create_post(posts: dict = Body(...)):
-    return{"new_post": f"title {posts.title} content: {posts.content}"}
+def create_post(new_post: Post):
+    print(new_post.title)
+    return{"data": "new post", }
+
+    
