dff9f18036827468acc78b5114821c20b8d16fcc
Baygeldi <baygeldi0718@gmail.com>
2026-05-19 21:45:19 +0800

    Code based database added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
+
 
 app = FastAPI()
 
@@ -10,17 +12,22 @@ class Post(BaseModel):
     content: str
     rating: Optional[int]
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "Favorite meal and fruits", "content": "Watermelon", "id": 2}]
+
 @app.get("/")
 async def root():
     return {"Welcome": "TO My API"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 @app.post("/posts")
 def create_post(new_post: Post):
-    print(new_post.title)
-    return{"data": "new post", }
+    new_post_dict = new_post.dict()
+    new_post_dict['id'] = randrange(0,1000000)
+    my_posts.append(new_post_dict)
+    return{"data": new_post}
 
     
