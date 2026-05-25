6af4118973c60ff43df6e6a5e691b9e54f648dfa
Baygeldi <baygeldi0718@gmail.com>
2026-05-25 17:36:03 +0800

    Sqlalchemy ORM is used to connect to database

--- app/database/db.py
+++ app/database/db.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import settings
+#from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+SQL_ALCHEMY_URL = 'postgresql://postgres:password1234@localhost/backend-architecture'
+
+engine = create_engine(SQL_ALCHEMY_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
