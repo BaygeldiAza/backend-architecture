ef2963c46ee6a9de47cc72600d4d4bcb3469c07b
Baygeldi <baygeldi0718@gmail.com>
2026-05-25 18:33:34 +0800

    Database initilazed

--- app/database/db.py
+++ app/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 #from app.core.config import settings
 
-SQL_ALCHEMY_URL = 'postgresql://postgres:password1234@localhost/backend-architecture'
+SQL_ALCHEMY_URL = "postgresql://postgres:password@localhost:5432/backend-architecture"
 
 engine = create_engine(SQL_ALCHEMY_URL)
 
@@ -13,3 +13,10 @@ SessionLocal = sessionmaker(
 )
 
 Base=declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
