8603f94b1c1196bd2536ccf3c35137e0bbc7f7dd
Baygeldi <baygeldi0718@gmail.com>
2026-07-14 08:22:58 +0800

    Limit, skip, search request added

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import Response, HTTPException, status, Depends, APIRouter
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 
 from ..database.db import get_db
 from ..models import models
@@ -13,8 +13,8 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[Post])
-def get_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
+def get_posts(db: Session = Depends(get_db), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
