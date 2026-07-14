d23d9f48a93f98713ff5364a0630a313ced831ea
Baygeldi <baygeldi0718@gmail.com>
2026-07-14 18:57:43 +0800

    Refactor database configuration to use environment variables instead of hard-coded credentials

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,15 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+class Settings(BaseSettings):
+    DATABASE_HOSTNAME: str
+    DATABASE_PORT: str
+    DATABASE_USERNAME: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config =SettingsConfigDict(env_file=".env")
+
+settings = Settings()
