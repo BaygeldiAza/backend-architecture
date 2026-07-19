c6fe200580635c7a4211c0f0ea15d9816c97a075
Baygeldi <baygeldi0718@gmail.com>
2026-07-19 21:46:19 +0800

    Constraint added for Votes endpoint

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status, HTTPException, APIRouter, Depends
+from fastapi import status, HTTPException, APIRouter, Depends
 from sqlalchemy.orm import Session
 from ..schemas import schemas
 from ..models import models 
@@ -11,6 +11,10 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote( vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {vote.post_id} does not exist")
+    
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
     if (vote.dir == 1):
