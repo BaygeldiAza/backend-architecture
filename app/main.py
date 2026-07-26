8456e34fdbdef3b36948890188db2397ded1c60b
Baygeldi <baygeldi0718@gmail.com>
2026-07-26 20:19:44 +0800

    Server --reload command added

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ def get_settings():
 
 @app.get("/")
 async def root():
-    return {"Welcome": "to Fast API "}
+    return {"Welcome": "Bay API "}
 
 app.include_router(post.router)
 app.include_router(user.router)
